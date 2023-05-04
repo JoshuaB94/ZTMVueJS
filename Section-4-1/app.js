4fe61d8abb16c9fb008298f2ef1debeb3ac0b067
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2023-05-04 06:05:59 -0500

    Learned about the Vue Compiler + Created a Vue Component

--- Section-4-1/app.js
+++ Section-4-1/app.js
@@ -1,37 +1,30 @@
 let vm = Vue.createApp({
+    
+    // template: ``
+})
+
+vm.component('jb-hello', {
+    template: `<h1>{{ message }}</h1>`,
     data() {
         return {
-            message: "Hello World!"
+            message: "Hey What is Up!"
         }
-    },
-    beforeCreate() {
-        console.log('beforeCreate() function called!', this.message)
-    },
-    created() {
-        console.log('created() function called!', this.message)
-    },
-    beforeMount() {
-        console.log('beforeMount() function called!', this.$el)
-    },
-    mounted() {
-        console.log('mounted() function called!', this.$el)
-    },
-    beforeUpdate() {
-        console.log('beforeUpdate() function called!')
-    },
-    updated() {
-        console.log('updated() function called!')
-    },
-    beforeUnmount() {
-        console.log('beforeUnmount() function called!')
-    },
-    unmounted() {
-        console.log('unmounted() function called!')
     }
 })
 
 vm.mount('#app')
 
-// setTimeout(() => {
-//     vm.mount('#app')
-// }, 3000)
+
+// let vm2 = Vue.createApp({
+//     data() {
+//         return {
+//             message: "Hello Mundo!"
+//         }
+//     },
+//     render() {
+//         return Vue.h(
+//             'h1',
+//             this.message
+//         )
+//     }
+// }).mount("#app2")
