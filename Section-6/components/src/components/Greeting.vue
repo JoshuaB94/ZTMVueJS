5d7d47a5bea911d5154454e42c90bd9be0d87ab8
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2023-05-05 10:48:08 -0500

    Learned about Advanced Vue Components. Specifically creating a Vue Project, Creating Components, Child Components, Using SASS in Components, Component Comms, Props, Emmitting Events, Callback Funcs

---
+++ Section-6/components/src/components/Greeting.vue
@@ -0,0 +1,26 @@
+<template>
+    <p v-if="age > 25">{{ msg }}</p>
+    <p v-else>
+      You must be {{ age }} years or older to view this content.
+    </p>
+</template>
+
+<script>
+    export default {
+    name: "Greeting",
+    props: [
+      "age"
+    ],
+    data() {
+      return {
+        msg: "Hi, Joshua Berrios!"
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    p {
+        color: darken(rgb(0, 219, 0), 15%);
+    }
+</style>
