95e75fbfb01af19046bdb6acaf818576af04467d
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2023-05-05 16:43:33 -0500

    Learned about Slots and how to use them in Vue.js

---
+++ Section-6-2/slots/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <app-form>
+      <template v-slot:help>
+        <p>{{ help }}</p>
+      </template>
+
+      <template v-slot:fields>
+        <input type="email" placeholder="email"/>
+        <input type="text" placeholder="username"/>
+        <input type="password" placeholder="password"/>
+      </template>
+
+      <template v-slot:buttons>
+        <button type="submit">Submit</button>
+      </template>
+
+      <p>Dummy Text</p>
+    </app-form>
+    
+    <app-form>
+      <template v-slot:help>
+        <p>Contact some help text.</p>
+      </template>
+
+      <template v-slot:fields>
+        <input type="text" placeholder="name"/>
+        <textarea type="text" placeholder="enter your message here"/>
+      </template>
+
+      <template v-slot:buttons>
+        <button type="submit">Submit</button>
+      </template>
+    </app-form>
+</template>
+
+<script>
+  import AppForm from "./components/Form.vue";
+
+  export default {
+    name: "App",
+    components: {
+      AppForm,
+    },
+    data() {
+      return {
+        help: "This is some help text."
+      }
+    }
+  }
+</script>
