99465a440e6f61e70e4d225a61462bd06c161d7f
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2023-05-11 11:10:26 -0500

    Learned the basics of Reactive References within Vue.js

---
+++ Section-21-3/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>{{ num }}</p>
+    <button type="button" @click.prevent="increment">+</button>
+    <button type="button" @click.prevent="decrement">-</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "App",
+  setup() {
+    let num = ref(0);
+
+    function increment() {
+      num.value++;
+    }
+
+    function decrement() {
+      num.value--;
+    }
+
+    return {
+      num,
+      increment,
+      decrement,
+    };
+  },
+};
+</script>
