f2dd7e241e6312cd87b683cb3a2bd0346fdb9821
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2023-05-11 13:48:01 -0500

    Learned about Reactive Functions in Vue.js and how to also maintain the reactivity of a function while using a spread operator

--- Section-21-3/src/App.vue
+++ Section-21-3/src/App.vue
@@ -3,11 +3,13 @@
     <p>{{ num }}</p>
     <button type="button" @click.prevent="increment">+</button>
     <button type="button" @click.prevent="decrement">-</button>
+
+    <p>{{ name }}</p>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive, toRefs } from "vue";
 
 export default {
   name: "App",
@@ -22,10 +24,20 @@ export default {
       num.value--;
     }
 
+    const user = reactive({
+      name: "Joshua",
+      age: 29,
+    });
+
+    setTimeout(() => {
+      user.name = "Josh";
+    }, 3000);
+
     return {
       num,
       increment,
       decrement,
+      ...toRefs(user),
     };
   },
 };
