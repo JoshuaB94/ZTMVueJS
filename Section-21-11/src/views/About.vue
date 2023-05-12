1e50f20cae0a4a90a2e8a9331a8e968c257d788c
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2023-05-12 11:10:35 -0500

    Learned about Pinia Hooks in Vue.js

--- Section-21-11/src/views/About.vue
+++ Section-21-11/src/views/About.vue
@@ -1,22 +1,33 @@
 <template>
   <div class="about">
     <h1>{{ $route.params.member }}</h1>
+    <p>{{ store.counter }}</p>
+    <p>{{ store.doubleCount }}</p>
+    <button type="button" @click.prevent="store.increment">
+      Clicky Button
+    </button>
   </div>
 </template>
 
 <script>
 import { useRoute, useRouter } from "vue-router";
+import { useCounterStore } from "../stores/counter";
 
 export default {
   setup() {
     const route = useRoute();
     const router = useRouter();
+    const store = useCounterStore();
 
     console.log(route.params.member);
 
     router.push({
       hash: "#test",
     });
+
+    return {
+      store,
+    };
   },
 };
 </script>
